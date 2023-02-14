ee56a0ddcb6e196d0b1c2af3a2e23f18048723e1
elevyg <elevyg91@gmail.com>
2023-02-14 11:43:21 -0300

    feat: cooperativa scrapping

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,59 @@
 import axios from "axios"
+import cheerio from "cheerio"
 
-const url = "https://www.andescalada.org/politica-de-privacidad" // URL we're scraping
+const url = "https://www.cooperativa.cl" // URL we're scraping
 const AxiosInstance = axios.create() // Create a new Axios Instance
 
-// Send an async HTTP Get request to the url
-AxiosInstance.get(url)
-  .then(
-    // Once we have data returned ...
-    (response) => {
-      const html = response.data // Get the HTML from the HTTP request
-      console.log(html)
-    },
-  )
-  .catch(console.error) // Error handling
+const main = async () => {
+  try {
+    const landing = await AxiosInstance.get(url)
+    const html = landing.data
+    const $ = cheerio.load(html)
+    const viewMore = $("#a5").find("a:contains('Ver más')").attr("href")
+
+    const viewMoreUrl = await AxiosInstance.get(viewMore)
+    const root = cheerio.load(viewMoreUrl.data)
+    const articles = root(".titular-noticia")
+      .toArray()
+      .map((el) => {
+        return {
+          title: root(el).text().trim(),
+          href: root(el).parent().attr("href"),
+        }
+      })
+
+    const subArticles = await Promise.all(
+      articles.map(async (article) => {
+        const articleUrl = await AxiosInstance.get(url + article.href)
+        const loadArticle = cheerio.load(articleUrl.data)
+
+        const topics = loadArticle(".rotulo-topicos")
+          .find("a")
+          .toArray()
+          .map((el) => {
+            return loadArticle(el).text().replace("| ", "")
+          })
+        const title = loadArticle("h1")
+          .toArray()
+          .map((el) => {
+            return loadArticle(el).text()
+          })
+        const publishedDate = loadArticle(".fecha-publicacion")
+          .find("time")
+          .toArray()
+          .map((el) => loadArticle(el).text())
+        const author = loadArticle(".contenedor-datos:contains('Autor')")
+          .find("span")
+          .toArray()
+          .map((el) => loadArticle(el).text())
+        return { topics, title, publishedDate, author, url: url + article.href }
+      }),
+    )
+
+    console.log(subArticles)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
